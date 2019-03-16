04910a5a4868bf0665667f34508c5adaedd7af00
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-03-15 20:02:07 -0500

    added pokemon

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friendData = [
+    {
+        "scores" : [3,2,5,1,3,2,2,1,3,5],
+        "photo" : "https://cdn.bulbagarden.net/upload/e/e2/133Eevee.png",
+        "name": "Eevee"
+    },
+    {
+        "scores" : [1,3,4,1,4,3,5,3,5,4],
+        "image" : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+        "name": "Flareon"
+    },
+    {
+        "scores" : [5,5,4,5,5,3,5,3,5,1],
+        "image" : "https://cdn.bulbagarden.net/upload/a/a7/196Espeon.png",
+        "name": "Espeon"
+    },
+    {
+        "scores" : [3,2,5,1,3,2,2,1,3,5],
+        "photo" : "https://cdn.bulbagarden.net/upload/e/e8/700Sylveon.png",
+        "name": "Sylveon"
+    },
+    {
+        "scores" : [2,1,2,4,5,5,2,3,1,4],
+        "photo" : "https://cdn.bulbagarden.net/upload/f/fd/134Vaporeon.png",
+        "name": "Vaporeon"
+    },
+    {
+        "scores" : [4,1,3,5,4,2,5,2,1,3],
+        "photo" : "https://cdn.bulbagarden.net/upload/b/bb/135Jolteon.png",
+        "name": "Jolteon"
+    },
+    {
+        "scores" : [3,2,1,4,5,6,2,1,3,5],
+        "photo" : "https://cdn.bulbagarden.net/upload/3/3d/197Umbreon.png",
+        "name": "Umbreon"
+    },
+    {
+        "scores" : [1,2,4,3,2,5,4,5,1,2],
+        "photo" : "https://cdn.bulbagarden.net/upload/2/23/471Glaceon.png",
+        "name": "Glaceon"
+    },
+    ];
+    
+// export to other modules
+    module.exports = friendData;
