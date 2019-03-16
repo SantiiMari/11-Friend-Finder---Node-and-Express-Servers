9babd3cb6c84d2ae413295139a9b79b68a0e5d85
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-03-15 23:28:44 -0500

    images wont work and survey will not submit 505 error

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,43 +1,51 @@
 var friendArr = [
     {
-        "scores" : [3,2,5,1,3,2,2,1,3,5],
+        
         "photo" : "https://cdn.bulbagarden.net/upload/e/e2/133Eevee.png",
-        "name": "Eevee"
+        "name": "Eevee",
+        "scores" : [3,2,5,1,3,2,2,1,3,5]
     },
     {
-        "scores" : [1,3,4,1,4,3,5,3,5,4],
+        
         "image" : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-        "name": "Flareon"
+        "name": "Flareon",
+        "scores" : [1,3,4,1,4,3,5,3,5,4]
     },
     {
-        "scores" : [5,5,4,5,5,3,5,3,5,1],
+        
         "image" : "https://cdn.bulbagarden.net/upload/a/a7/196Espeon.png",
-        "name": "Espeon"
+        "name": "Espeon",
+        "scores" : [5,5,4,5,5,3,5,3,5,1]
     },
     {
-        "scores" : [3,2,5,1,3,2,2,1,3,5],
+        
         "photo" : "https://cdn.bulbagarden.net/upload/e/e8/700Sylveon.png",
-        "name": "Sylveon"
+        "name": "Sylveon",
+        "scores" : [3,2,5,1,3,2,2,1,3,5],
     },
     {
-        "scores" : [2,1,2,4,5,5,2,3,1,4],
+        
         "photo" : "https://cdn.bulbagarden.net/upload/f/fd/134Vaporeon.png",
-        "name": "Vaporeon"
+        "name": "Vaporeon",
+        "scores" : [2,1,2,4,5,5,2,3,1,4]
     },
     {
-        "scores" : [4,1,3,5,4,2,5,2,1,3],
+        
         "photo" : "https://cdn.bulbagarden.net/upload/b/bb/135Jolteon.png",
-        "name": "Jolteon"
+        "name": "Jolteon",
+        "scores" : [4,1,3,5,4,2,5,2,1,3],
     },
     {
-        "scores" : [3,2,1,4,5,6,2,1,3,5],
+        
         "photo" : "https://cdn.bulbagarden.net/upload/3/3d/197Umbreon.png",
-        "name": "Umbreon"
+        "name": "Umbreon",
+        "scores" : [3,2,1,4,5,6,2,1,3,5],
     },
     {
-        "scores" : [1,2,4,3,2,5,4,5,1,2],
+        
         "photo" : "https://cdn.bulbagarden.net/upload/2/23/471Glaceon.png",
-        "name": "Glaceon"
+        "name": "Glaceon",
+        "scores" : [1,2,4,3,2,5,4,5,1,2]
     },
     ];
     
